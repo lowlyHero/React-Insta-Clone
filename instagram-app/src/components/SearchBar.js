f0bdb909b3b3464d2182387250b595f1ce4b129e
lowlyHero <e.nunn1009@gmail.com>
2019-05-20 18:30:20 -0400

    All items for Searchbar rendered, moving onto PostContainer

--- instagram-app/src/components/SearchBar.js
+++ instagram-app/src/components/SearchBar.js
@@ -6,6 +6,9 @@ const SearchBar = () => {
     <div>
       <i className='fab fa-instagram fa-2x'> | Instagram</i>
       <input type ='text' placeholder ='Search'></input> 
+      <i class="far fa-compass"></i>
+      <i class="far fa-heart"></i>
+      <i class="far fa-user"></i>
       <hr />
     </div>
   );
