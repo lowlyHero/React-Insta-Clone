addaa61c9c8da3b6389896ae092938ffb3873aec
lowlyHero <e.nunn1009@gmail.com>
2019-05-22 17:59:40 -0400

    Getting like button to increment number of likes

--- instagram-app/src/components/PostComponents/LikeContainer.js
+++ instagram-app/src/components/PostComponents/LikeContainer.js
@@ -4,23 +4,24 @@ import React from 'react';
 //Functions to be done in Parent contianer and invoked here
 
 const LikeContainer = props => {
-    return (
+    return [
+        <div className='like-section'>
         <div
-          className="like-section"
-          key="likes-icons-container"
+          key='likes-icons-container'
           onClick={props.incrementLike}
         >
-          <div className="like-section-wrapper">
-            <i className="far fa-heart" />
+          <div className='like-section-wrapper'>
+            <i className='far fa-heart' />
           </div>
-          <div className="like-section-wrapper">
-            <i className="far fa-comment" />
-          </div>
-        </div>,
-        <div className="like-section" key="likes-container">
-          <div className="like-section-wrapper">{props.likes}</div>
         </div>
-    );
+          <div className='like-section-wrapper'>
+            <i className='far fa-comment' />
+          </div>
+          <div className='like-section' key='likes-container'>
+           <div className='like-section-wrapper'>{props.likes}</div>
+          </div>
+        </div>
+    ];
 };
 
 export default LikeContainer
